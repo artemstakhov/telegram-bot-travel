b3862364f12160ccf78e1db8b2e2e88437fe9f78
newnoope <artyomstahov33@gmail.com>
2023-06-29 01:21:54 +0300

    eslint react-plugin and config +-

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,31 +1,26 @@
 module.exports = {
 	env: {
 		browser: true,
+		commonjs: true,
 		es2021: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	overrides: [],
 	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2021,
-		sourceType: 'module',
+		ecmaVersion: 'latest',
+		'sourceType': 'module',
 	},
-	plugins: ['react'],
 	rules: {
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
-		'react/prop-types': 'off', // Опционально: отключить предупреждения о propTypes
-	},
-	globals: {
-		React: 'writable',
-		module: 'readonly',
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
+		// suppress errors for missing 'import React' in files
+		'react/react-in-jsx-scope': 'off',
+		// allow jsx syntax in js files (for next.js project)
+		'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }], //should add ".ts" if typescript project
 	},
 };
